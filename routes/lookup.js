cff011663e5c9cfb235f4c3b77844d7a76ac54e1
scottcwang <wangsc@cs.wisc.edu>
2020-08-14 14:31:53 -0500

    Parse Host in HTTP header to obtain username and service

--- routes/lookup.js
+++ routes/lookup.js
@@ -2,6 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
+  let [username, service, ...hostname] = req.hostname.split('.');
+
+  if (hostname.join('.') !== process.env.PKS_HOSTNAME) {
+    throw new Error(
+      'Specify both username and service: <username>.<service>.'
+      + process.env.PKS_HOSTNAME
+    );
+  }
+
   res.render('index', { title: 'Express' });
 });
 
