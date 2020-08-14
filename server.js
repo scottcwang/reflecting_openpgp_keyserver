c30dee883ec5094615a8c55835f11e73a76b4abd
scottcwang <wangsc@cs.wisc.edu>
2020-08-13 22:20:43 -0500

    Remove unused server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-require('dotenv').config();
-
-var express_app = require('./app');
-
-express_app.listen(process.env.PORT, process.env.HOST);
-console.log(`Running on http://${process.env.HOST}:${process.env.PORT}`);
