765d2ab40a5f88787327273fa711ac5d3d57cba6
scottcwang <wangsc@cs.wisc.edu>
2020-08-14 16:03:09 -0500

    Add request to GitHub API

--- routes/lookup.js
+++ routes/lookup.js
@@ -1,7 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
+var axios = require('axios');
+
+serviceReqEnum = Object.freeze({
+  'github': requestGitHub
+})
+
+async function requestGitHub(username) {
+  try {
+    let url = 'https://github.com/' + username + '.gpg'
+    let resp = await axios.get(url);
+    return resp;
+  } catch (error) {
+    return error;
+  }
+}
+
+router.get('/', async function (req, res, next) {
   let [username, service, ...hostname] = req.hostname.split('.');
 
   if (hostname.join('.') !== process.env.PKS_HOSTNAME) {
@@ -10,6 +26,8 @@ router.get('/', function (req, res, next) {
       + process.env.PKS_HOSTNAME
     );
   }
+
+  let serviceRes = await (serviceReqEnum[service])(username);
 
   res.render('index', { title: 'Express' });
 });
