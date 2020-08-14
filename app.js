0c127eb8f9fc43080a7d6d20032cd60543311694
scottcwang <wangsc@cs.wisc.edu>
2020-08-13 22:16:45 -0500

    Move dotenv load into app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var lookupRouter = require('./routes/lookup');
 
+require('dotenv').config();
+
 var app = express();
 
 // view engine setup
@@ -21,12 +23,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/pks/lookup', lookupRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
